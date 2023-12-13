de922dd7fecf3b3093401e771b326ebd1e4394cc
Naoki-Noto <118686232+Naoki-Noto@users.noreply.github.com>
2023-12-13 14:00:40 +0900

    Add files via upload

---
+++ ML/Conventional_ML/RF/RF.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 22:51:13 2021
+
+@author: notonaoki
+"""
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
+### Choose dataset ###
+dataset = 'DFT'
+#dataset = 'MF'
+#dataset = 'MK'
+#dataset = 'MD'
+#dataset = 'mordred'
+#dataset = 'MF_pca'
+#dataset = 'MK_pca'
+#dataset = 'MD_pca'
+#dataset = 'mordred_pca'
+#dataset = 'DFT_MD_pca'
+#dataset = 'DFT_MK_pca'
+#dataset = 'DFT_mordred_pca'
+#dataset = 'DFT_MF_pca'
+
+data = pd.read_csv('data/data_{}.csv'.format(dataset))
+y = pd.DataFrame(data['Yield'],columns=['Yield'])
+print(y)
+X = data.drop(columns=['Name', 'ID', 'Yield'])
+print(X)
+
+
+r2_train = []
+r2_test = []
+mae_train = []
+mae_test = []
+for i in range(0,100):
+    print(i)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=i)
+    param = {"n_estimators": [1000, 3000, 5000], "max_depth": [3, 4, 5, 6]}
+    reg = GridSearchCV(RandomForestRegressor(random_state=0), param_grid=param, cv=10, n_jobs=16)
+    reg.fit(X_train,y_train['Yield'])
+    best = reg.best_estimator_
+    print(reg.best_estimator_)
+    y_pred1 = best.predict(X_train)
+    y_pred2 = best.predict(X_test)
+    r2_train.append(metrics.r2_score(y_train, y_pred1))
+    r2_test.append(metrics.r2_score(y_test, y_pred2))
+    mae_train.append(metrics.mean_absolute_error(y_train, y_pred1))
+    mae_test.append(metrics.mean_absolute_error(y_test, y_pred2))
+    
+
+r2_train = pd.DataFrame(data=r2_train, columns=['r2_train'])
+r2_test = pd.DataFrame(data=r2_test, columns=['r2_test'])
+mae_train = pd.DataFrame(data=mae_train, columns=['mae_train'])
+mae_test = pd.DataFrame(data=mae_test, columns=['mae_test'])
+result = pd.concat([r2_train, r2_test, mae_train, mae_test], axis=1, join='inner')
+result.to_csv('result/result_{}.csv'.format(dataset))
