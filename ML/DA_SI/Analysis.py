fef25423258216c0216178b8901d1a3492dd6813
Naoki N <118686232+Naoki-Noto@users.noreply.github.com>
2024-03-06 20:19:08 +0900

    Add files via upload

---
+++ ML/DA_SI/Analysis.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 09:56:28 2023
+
+@author: noton
+"""
+
+import pandas as pd
+
+data = pd.read_csv('result/result_FA.csv')
+data = data.drop(columns=['Unnamed: 0', 'r2_all', 'mae_all', 'r2_lab', 'mae_lab',
+                          #'unsupervised',
+                          ])
+#print(data)
+
+data_max = data.max()
+data_min = data.min()
+data_mean = data.mean()
+data_std = data.std(ddof=0)
+
+print('max:')
+print(data_max)
+print('min:')
+print(data_min)
+print('mean:')
+print(data_mean)
+print('std:')
+print(data_std)
